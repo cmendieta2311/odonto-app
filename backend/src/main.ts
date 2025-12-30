5e911bcc792fd743ccb790e46be9b303ecc43e4f
cmendieta2311@gmail.com <cmendieta2311@gmail.com>
2025-12-30 14:01:07 -0300

    Fix: Bind backend to 0.0.0.0 to ensure Docker connectivity

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
